3ef9e00cc8909ff47a14752df7a911418e00a497
mazkaaz <azka.azzahidin@gmail.com>
2019-03-22 16:41:34 +0700

    edit group service ke DB baru

--- models/device_group.js
+++ models/device_group.js
@@ -3,7 +3,7 @@
 module.exports = function (sequelize, Sequelize) {
 	var device_group = sequelize.define('device_group', {
 		id: { type: Sequelize.INTEGER, allowNull: false, unique: true, primaryKey: true, autoIncrement: true },
-		id_akun: { type: Sequelize.STRING, allowNull: false},
+		user_id: { type: Sequelize.STRING, allowNull: false},
 		name: { type: Sequelize.STRING, allowNull: false},
 		description: { type: Sequelize.STRING, allowNull: true},
 		tdl: { type: Sequelize.INTEGER, allowNull: true}
