c28175b35ed2efcfaa743c98389eb9f8a3bf171c
Yunizar Isnaini <nizaar.voltric@gmail.com>
2019-12-02 19:27:42 +0700

    fixing roles permission

--- functions/roles.js
+++ functions/roles.js
@@ -1,3 +1,4 @@
+const async =  require('async')
 let query = {}
 
 function feature(req, level_id) {
@@ -19,36 +20,49 @@ function feature(req, level_id) {
 }
 
 function can(req, role, cb) {
-    let response = {}
-    query.level_id = {
-        where: { user_id: req.auth.user_id },
-        attributes: ['level_id'],
-        include: 'user_level'
-    }
+    async.waterfall([
+        function checkingCondition(cb) {
+            if (req.auth) {
+                cb(null, req.auth.user_level)
+            } else if (req.body.user_id) {
+                query.level_id = {
+                    where: { user_id: req.body.user_id },
+                    attributes: ['level_id'],
+                    include: 'user_level'
+                }
 
-    req.APP.models.mysql.user.findOne(query.level_id)
-    .then((result) => {
-        if (!result.level_id) throw new Error('LEVEL_EMPTY')
+                req.APP.models.mysql.user.findOne(query.level_id)
+                    .then((result) => {
+                        if (!result.level_id) throw new Error('LEVEL_EMPTY')
 
-        let level_previledge = result.user_level.level_previledge
-        let mapFeature = feature(req, level_previledge)
+                        let level_previledge = result.user_level.level_previledge
+                        let mapFeature = feature(req, level_previledge)
 
-        Promise.all(mapFeature).then((result) => {
-            (result.indexOf(role) >= 0) ? response.granted = true : response.granted = false;
-
-            return cb(null, response);
-        }).catch((err) => {
-            throw new Error(err)
-        });
-    }).catch((err) => {
-        if (err.message == 'LEVEL_EMPTY') {
-            return cb(null, { granted: true });
+                        Promise.all(mapFeature).then((result) => cb(null, result))
+                            .catch((err) => {
+                                throw new Error(err)
+                            });
+                    }).catch((err) => {
+                        if (err.message == 'LEVEL_EMPTY') {
+                            cb(null, { granted: true })
+                        } else {
+                            return cb({
+                                code: 'GENERAL_ERR',
+                                message: err.message
+                            })
+                        }
+                    });
+            } else {
+                cb(null, { granted: true })
+            }
+        }
+    ], function (err, permission) {
+        if (err) return cb(err)
+        if (permission.granted || permission.indexOf(role) >= 0) {
+            return cb(null, { granted: true })
         }
 
-        return cb({
-            code: 'GENERAL_ERR',
-            message: err.message
-        })
+        return cb(null, { granted: false })
     });
 }
 
